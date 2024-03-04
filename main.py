aaa765b69a3233c15af801ab653ce724cf0fda0e
hoangluong202 <hl01012002@gmail.com>
2024-03-04 08:20:50 +0700

    fix(lab1): wait successful noti before publish data

--- main.py
+++ main.py
@@ -10,46 +10,48 @@ load_dotenv()  # take environment variables from .env.
 #Variables
 ADAFRUIT_IO_USERNAME = os.getenv("ADAFRUIT_IO_USERNAME")
 ADAFRUIT_IO_KEY = os.getenv("ADAFRUIT_IO_KEY")
-print('User name:',ADAFRUIT_IO_USERNAME)
 AIO_FEED_ID_BUTTON_1 = "button1"
 AIO_FEED_ID_BUTTON_2 = "button2"
 AIO_FEED_ID_SENSOR_1 = "sensor1"
 AIO_FEED_ID_SENSOR_2 = "sensor2"
 AIO_FEED_ID_SENSOR_3 = "sensor3"
 
-#Functions
 def connected(client):
-  print('Connected to Adafruit IO!')
-  client.subscribe(AIO_FEED_ID_BUTTON_1)
-  client.subscribe(AIO_FEED_ID_BUTTON_2)
+    print("Ket noi thanh cong ...")
+    client.subscribe(AIO_FEED_ID_BUTTON_1)
+    client.subscribe(AIO_FEED_ID_BUTTON_2)
 
-def subcribe(client, userdata, mid, granted_qos):
-  print('Subscribed to {0} with QoS {1}'.format(mid, granted_qos[0]))
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
 
 def disconnected(client):
-  print('Disconnected from Adafruit IO!')
-  sys.exit(1)
+    print("Ngat ket noi ...")
+    sys.exit (1)
 
-def message(client, feed_id, payload):
-  print('Feed {0} received new value: {1}'.format(feed_id, payload))
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload)
 
-
-client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+client = MQTTClient(ADAFRUIT_IO_USERNAME , ADAFRUIT_IO_KEY)
 client.on_connect = connected
 client.on_disconnect = disconnected
 client.on_message = message
-client.on_subscribe = subcribe
+client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
+while not client.is_connected():
+    print('Waiting for connection...')
+    time.sleep(1)
+
 while True:
+    time.sleep(10)
     temp = random.randint(0, 50)
     hum = random.randint(0, 100)
     light = random.randint(0, 100)
-    print('Publishing {0} to {1}.'.format(temp, AIO_FEED_ID_SENSOR_1))
+    print('Cap nhat cam bien nhiet do: {0}.'.format(temp))
     client.publish(AIO_FEED_ID_SENSOR_1, temp)
-    print('Publishing {0} to {1}.'.format(hum, AIO_FEED_ID_SENSOR_2))
+    print('Cap nhat cam bien do am: {0}.'.format(hum))
     client.publish(AIO_FEED_ID_SENSOR_2, hum)
-    print('Publishing {0} to {1}.'.format(light, AIO_FEED_ID_SENSOR_3))
+    print('Cap nhat cam bien anh sang: {0}.'.format(light))
     client.publish(AIO_FEED_ID_SENSOR_3, light)
-    time.sleep(10)
+    
